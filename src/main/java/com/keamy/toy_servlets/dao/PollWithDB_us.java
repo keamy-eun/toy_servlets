5ad049d3c1249d75b4e43e4d3402a6f1cb002afd
keamy-eun <eun.wonki014@gmail.com>
2022-12-22 15:18:51 +0900

    jsp

--- src/main/java/com/keamy/toy_servlets/dao/PollWithDB_us.java
+++ src/main/java/com/keamy/toy_servlets/dao/PollWithDB_us.java
@@ -3,27 +3,41 @@ package com.keamy.toy_servlets.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PollWithDB_us {
-    public HashMap<String, Object> getQuestion(String questionsUid) throws SQLException{
+    public ArrayList<Object> getQuestion(String questionsUid) throws SQLException{
 
         Commons commons = new Commons();
         Statement statement = commons.getStatement();
-
-        String query = "SELECT * FROM QUESTIONS_LIST " +
+        
+        ArrayList<Object> bundle_list = new ArrayList<>();
+        String query1 = "SELECT * FROM QUESTIONS_LIST " +
                         " WHERE QUESTIONS_UID = '"+questionsUid+"'";
+                        
+        HashMap<String, Object> question_list = new HashMap<>();
         
-        HashMap<String, Object> result = new HashMap<>();
-        
-        ResultSet resultSet = statement.executeQuery(query);
+        ResultSet resultSet = statement.executeQuery(query1);
         while(resultSet.next()){
-            result.put("QUESTIONS_UID",resultSet.getString("QUESTIONS_UID"));
-            result.put("QUESTIONS", resultSet.getString("QUESTIONS"));
-            result.put("ORDERS",resultSet.getInt("ORDERS"));
+            question_list.put("QUESTIONS_UID",resultSet.getString("QUESTIONS_UID"));
+            question_list.put("QUESTIONS", resultSet.getString("QUESTIONS"));
+            question_list.put("ORDERS",resultSet.getInt("ORDERS"));
         }
+        bundle_list.add(question_list);
+
+        String query2 = "SELECT * FROM EXAMPLE_LIST ";
+        ArrayList<String> example_list = new ArrayList<>();
+        ResultSet resultSet2 = statement.executeQuery(query2);
+        while(resultSet2.next()){
+            example_list.add(resultSet2.getString("EXAMPLE"));
+            System.out.println(resultSet2.getString("EXAMPLE"));
+        }
+        bundle_list.add(example_list);
+
         resultSet.close();
+        resultSet2.close();
         statement.close();
-        return result;
+        return bundle_list;
     }
 }
